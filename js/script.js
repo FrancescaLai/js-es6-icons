08fcd752924f20db336fadd4ed1b5b308e685b99
FrancescaLai <francescalai1690@gmail.com>
2021-03-23 17:15:13 +0100

    WIP

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-// 
-// Milestone 2
-// Coloriamo le icone per tipo
-// 
-// Milestone 3
-// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+// 1. Creare una struttura dati che contenga una lista di icone e mostrarle in una pagina
 
 const icons = [
   {
@@ -117,3 +110,110 @@ const icons = [
     category: "animal"
   },
 ];
+
+const iconsContainer = $(".icons");
+
+icons.forEach((icon) => {
+
+  const {name, family, prefix} = icon;
+
+  const html = `
+  <div>
+    <i class="${family} ${prefix} ${name}"></i>
+    <div class="title">${name}</div>
+  </div>
+  `;
+
+  iconsContainer.append(html);
+
+});
+
+// 2. Coloriamo le icone per tipo
+
+const colors = [
+  "yellow",
+  "coral",
+  "green"
+];
+
+const categories = [];
+
+icons.forEach((item, i) => {
+  if ( categories.includes(item.category) == false) {
+    categories.push(item.category);
+  }
+});
+
+const iconsColored = icons.map((icon) => {
+  const categoryIndex = categories.indexOf(icon.category);
+  const colorItem = colors[categoryIndex];
+
+  icon.color = colorItem;
+
+  return icon;
+});
+
+iconsColored.forEach((icon) => {
+
+  const {name, family, prefix, color} = icon;
+
+  const html = `
+  <div>
+    <i class="${family} ${prefix} ${name}" style="color: ${color}"></i>
+    <div class="title">${name}</div>
+  </div>
+  `;
+
+  iconsContainer.append(html);
+
+});
+
+// 3. Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+const select = $("#type");
+
+categories.forEach((item) => {
+  const optionHtml = `<option value="${item}">${item}</option>`;
+  select.append(optionHtml);
+});
+
+select.change(function(){
+  const optionSelected = $(this).val();
+
+  let iconsFiltered = iconsColored.filter((icon) => {
+
+    return icon.category == optionSelected;
+  });
+
+  if ( iconsFiltered.length == 0) {
+    iconsFiltered = iconsColored;
+  }
+
+  printIcons(iconsContainer, iconsFiltered);
+
+});
+
+
+// FUNZIONE GENERICA
+
+function printIcons(target, icons){
+
+  target.html("");
+
+  icons.forEach((icon) => {
+    const {name, family, prefix, color} = icon;
+
+    const html = `
+    <div>
+      <i class="${family} ${prefix} ${name}" style="color: ${color}"></i>
+      <div class="title">${name}</div>
+    </div>
+    `;
+
+    target.append(html);
+  });
+}
+
+function getCategories(){
+
+}
